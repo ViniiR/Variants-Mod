14c6a626b22d134447378c3d4598acc7812b2598
ViniiR <cvrm2004@gmail.com>
2026-04-25 20:49:54 -0300

    feat: add blocks to fuel registry

--- src/main/java/com/vinii/v2m/block/ModBlocks.java
+++ src/main/java/com/vinii/v2m/block/ModBlocks.java
@@ -5,6 +5,8 @@ import com.vinii.v2m.block.blocks.ModBarrelBlock;
 import com.vinii.v2m.block.blocks.ModChestBlock;
 import com.vinii.v2m.block.blocks.ModCraftingTableBlock;
 import com.vinii.v2m.block.blocks.ModFurnaceBlock;
+import com.vinii.v2m.datagen.ModBlockTagProvider;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -14,6 +16,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.IronBarsBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -26,6 +29,61 @@ import static net.minecraft.world.level.block.Blocks.litBlockEmission;
 
 public class ModBlocks {
     public static void initialize() {
+        FuelRegistryEvents.BUILD.register((builder, ctx) -> {
+                final int CRAFTING_TABLE_BURN_TIME = 300;
+                final int CHEST_BURN_TIME = 300;
+                final int BARREL_BURN_TIME = 300;
+
+                builder.add(SPRUCE_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(BIRCH_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(DARK_OAK_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(PALE_OAK_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(ACACIA_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(JUNGLE_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(MANGROVE_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(CHERRY_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(BAMBOO_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(CRIMSON_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+                builder.add(WARPED_CRAFTING_TABLE, CRAFTING_TABLE_BURN_TIME);
+
+                builder.add(SPRUCE_CHEST, CHEST_BURN_TIME);
+                builder.add(BIRCH_CHEST, CHEST_BURN_TIME);
+                builder.add(DARK_OAK_CHEST, CHEST_BURN_TIME);
+                builder.add(PALE_OAK_CHEST, CHEST_BURN_TIME);
+                builder.add(ACACIA_CHEST, CHEST_BURN_TIME);
+                builder.add(JUNGLE_CHEST, CHEST_BURN_TIME);
+                builder.add(MANGROVE_CHEST, CHEST_BURN_TIME);
+                builder.add(CHERRY_CHEST, CHEST_BURN_TIME);
+                builder.add(BAMBOO_CHEST, CHEST_BURN_TIME);
+                builder.add(CRIMSON_CHEST, CHEST_BURN_TIME);
+                builder.add(WARPED_CHEST, CHEST_BURN_TIME);
+
+                // TODO trapped chests
+//            builder.add(SPRUCE_CHEST, 200);
+//            builder.add(BIRCH_CHEST, 200);
+//            builder.add(DARK_OAK_CHEST, 200);
+//            builder.add(PALE_OAK_CHEST, 200);
+//            builder.add(ACACIA_CHEST, 200);
+//            builder.add(JUNGLE_CHEST, 200);
+//            builder.add(MANGROVE_CHEST, 200);
+//            builder.add(CHERRY_CHEST, 200);
+//            builder.add(BAMBOO_CHEST, 200);
+//            builder.add(CRIMSON_CHEST, 200);
+//            builder.add(WARPED_CHEST, 200);
+
+                builder.add(OAK_BARREL, BARREL_BURN_TIME);
+                builder.add(BIRCH_BARREL, BARREL_BURN_TIME);
+                builder.add(DARK_OAK_BARREL, BARREL_BURN_TIME);
+                builder.add(PALE_OAK_BARREL, BARREL_BURN_TIME);
+                builder.add(ACACIA_BARREL, BARREL_BURN_TIME);
+                builder.add(JUNGLE_BARREL, BARREL_BURN_TIME);
+                builder.add(MANGROVE_BARREL, BARREL_BURN_TIME);
+                builder.add(CHERRY_BARREL, BARREL_BURN_TIME);
+                builder.add(BAMBOO_BARREL, BARREL_BURN_TIME);
+                builder.add(CRIMSON_BARREL, BARREL_BURN_TIME);
+                builder.add(WARPED_BARREL, BARREL_BURN_TIME);
+            }
+        );
     }
 
     // TODO: set fuel registry for wood items
@@ -34,56 +92,54 @@ public class ModBlocks {
     public static final Block FROSTED_GLASS_PANE = glassPaneRegister("frosted_glass_pane");
     public static final Block SANDY_GLASS_PANE = glassPaneRegister("sandy_glass_pane");
 
-    // TODO: set map color individually
-
     // OAK is vanilla but modified textures name and recipe
-    public static final Block SPRUCE_CRAFTING_TABLE = craftingTableRegister("spruce_crafting_table", true);
-    public static final Block BIRCH_CRAFTING_TABLE = craftingTableRegister("birch_crafting_table", true);
-    public static final Block DARK_OAK_CRAFTING_TABLE = craftingTableRegister("dark_oak_crafting_table", true);
-    public static final Block PALE_OAK_CRAFTING_TABLE = craftingTableRegister("pale_oak_crafting_table", true);
-    public static final Block ACACIA_CRAFTING_TABLE = craftingTableRegister("acacia_crafting_table", true);
-    public static final Block JUNGLE_CRAFTING_TABLE = craftingTableRegister("jungle_crafting_table", true);
-    public static final Block MANGROVE_CRAFTING_TABLE = craftingTableRegister("mangrove_crafting_table", true);
-    public static final Block CHERRY_CRAFTING_TABLE = craftingTableRegister("cherry_crafting_table", true);
-    public static final Block BAMBOO_CRAFTING_TABLE = craftingTableRegister("bamboo_crafting_table", true);
-    public static final Block CRIMSON_CRAFTING_TABLE = craftingTableRegister("crimson_crafting_table", false);
-    public static final Block WARPED_CRAFTING_TABLE = craftingTableRegister("warped_crafting_table", false);
+    public static final Block SPRUCE_CRAFTING_TABLE = craftingTableRegister("spruce_crafting_table", MapColor.PODZOL, true);
+    public static final Block BIRCH_CRAFTING_TABLE = craftingTableRegister("birch_crafting_table", MapColor.SAND, true);
+    public static final Block DARK_OAK_CRAFTING_TABLE = craftingTableRegister("dark_oak_crafting_table", MapColor.COLOR_BROWN, true);
+    public static final Block PALE_OAK_CRAFTING_TABLE = craftingTableRegister("pale_oak_crafting_table", MapColor.QUARTZ, true);
+    public static final Block ACACIA_CRAFTING_TABLE = craftingTableRegister("acacia_crafting_table", MapColor.COLOR_ORANGE, true);
+    public static final Block JUNGLE_CRAFTING_TABLE = craftingTableRegister("jungle_crafting_table", MapColor.DIRT, true);
+    public static final Block MANGROVE_CRAFTING_TABLE = craftingTableRegister("mangrove_crafting_table", MapColor.COLOR_RED, true);
+    public static final Block CHERRY_CRAFTING_TABLE = craftingTableRegister("cherry_crafting_table", MapColor.TERRACOTTA_WHITE, true);
+    public static final Block BAMBOO_CRAFTING_TABLE = craftingTableRegister("bamboo_crafting_table", MapColor.COLOR_YELLOW, true);
+    public static final Block CRIMSON_CRAFTING_TABLE = craftingTableRegister("crimson_crafting_table", MapColor.CRIMSON_STEM, false);
+    public static final Block WARPED_CRAFTING_TABLE = craftingTableRegister("warped_crafting_table", MapColor.WARPED_STEM, false);
 
     // TODO: register trapped chests too
     // OAK is vanilla but modified textures name and recipe
-    public static final Block SPRUCE_CHEST = chestRegister("spruce_chest", true);
-    public static final Block BIRCH_CHEST = chestRegister("birch_chest", true);
-    public static final Block DARK_OAK_CHEST = chestRegister("dark_oak_chest", true);
-    public static final Block PALE_OAK_CHEST = chestRegister("pale_oak_chest", true);
-    public static final Block ACACIA_CHEST = chestRegister("acacia_chest", true);
-    public static final Block JUNGLE_CHEST = chestRegister("jungle_chest", true);
-    public static final Block MANGROVE_CHEST = chestRegister("mangrove_chest", true);
-    public static final Block CHERRY_CHEST = chestRegister("cherry_chest", true);
-    public static final Block BAMBOO_CHEST = chestRegister("bamboo_chest", true);
-    public static final Block CRIMSON_CHEST = chestRegister("crimson_chest", false);
-    public static final Block WARPED_CHEST = chestRegister("warped_chest", false);
+    public static final Block SPRUCE_CHEST = chestRegister("spruce_chest", MapColor.PODZOL, true);
+    public static final Block BIRCH_CHEST = chestRegister("birch_chest", MapColor.SAND, true);
+    public static final Block DARK_OAK_CHEST = chestRegister("dark_oak_chest", MapColor.COLOR_BROWN, true);
+    public static final Block PALE_OAK_CHEST = chestRegister("pale_oak_chest", MapColor.QUARTZ, true);
+    public static final Block ACACIA_CHEST = chestRegister("acacia_chest", MapColor.COLOR_ORANGE, true);
+    public static final Block JUNGLE_CHEST = chestRegister("jungle_chest", MapColor.DIRT, true);
+    public static final Block MANGROVE_CHEST = chestRegister("mangrove_chest", MapColor.COLOR_RED, true);
+    public static final Block CHERRY_CHEST = chestRegister("cherry_chest", MapColor.TERRACOTTA_WHITE, true);
+    public static final Block BAMBOO_CHEST = chestRegister("bamboo_chest", MapColor.COLOR_YELLOW, true);
+    public static final Block CRIMSON_CHEST = chestRegister("crimson_chest", MapColor.CRIMSON_STEM, false);
+    public static final Block WARPED_CHEST = chestRegister("warped_chest", MapColor.WARPED_STEM, false);
 
     // TODO: make all barrels valid profession blocks
     // SPRUCE is vanilla but modified textures name and recipe
-    public static final Block OAK_BARREL = barrelRegister("oak_barrel", true);
-    public static final Block BIRCH_BARREL = barrelRegister("birch_barrel", true);
-    public static final Block DARK_OAK_BARREL = barrelRegister("dark_oak_barrel", true);
-    public static final Block PALE_OAK_BARREL = barrelRegister("pale_oak_barrel", true);
-    public static final Block ACACIA_BARREL = barrelRegister("acacia_barrel", true);
-    public static final Block JUNGLE_BARREL = barrelRegister("jungle_barrel", true);
-    public static final Block MANGROVE_BARREL = barrelRegister("mangrove_barrel", true);
-    public static final Block CHERRY_BARREL = barrelRegister("cherry_barrel", true);
-    public static final Block BAMBOO_BARREL = barrelRegister("bamboo_barrel", true);
-    public static final Block CRIMSON_BARREL = barrelRegister("crimson_barrel", false);
-    public static final Block WARPED_BARREL = barrelRegister("warped_barrel", false);
+    public static final Block OAK_BARREL = barrelRegister("oak_barrel", MapColor.WOOD, true);
+    public static final Block BIRCH_BARREL = barrelRegister("birch_barrel", MapColor.SAND, true);
+    public static final Block DARK_OAK_BARREL = barrelRegister("dark_oak_barrel", MapColor.COLOR_BROWN, true);
+    public static final Block PALE_OAK_BARREL = barrelRegister("pale_oak_barrel", MapColor.QUARTZ, true);
+    public static final Block ACACIA_BARREL = barrelRegister("acacia_barrel", MapColor.COLOR_ORANGE, true);
+    public static final Block JUNGLE_BARREL = barrelRegister("jungle_barrel", MapColor.DIRT, true);
+    public static final Block MANGROVE_BARREL = barrelRegister("mangrove_barrel", MapColor.COLOR_RED, true);
+    public static final Block CHERRY_BARREL = barrelRegister("cherry_barrel", MapColor.TERRACOTTA_WHITE, true);
+    public static final Block BAMBOO_BARREL = barrelRegister("bamboo_barrel", MapColor.COLOR_YELLOW, true);
+    public static final Block CRIMSON_BARREL = barrelRegister("crimson_barrel", MapColor.CRIMSON_STEM, false);
+    public static final Block WARPED_BARREL = barrelRegister("warped_barrel", MapColor.WARPED_STEM, false);
 
     // STONE is vanilla but modified textures name and recipe
-    public static final Block BLACKSTONE_FURNACE = furnaceRegister("blackstone_furnace");
-    public static final Block DEEPSLATE_FURNACE = furnaceRegister("deepslate_furnace");
+    public static final Block BLACKSTONE_FURNACE = furnaceRegister("blackstone_furnace", MapColor.COLOR_BLACK);
+    public static final Block DEEPSLATE_FURNACE = furnaceRegister("deepslate_furnace", MapColor.DEEPSLATE);
 
-    private static Block chestRegister(String name, Boolean burns) {
+    private static Block chestRegister(String name, MapColor mapColor, Boolean burns) {
         BlockBehaviour.Properties properties = BlockBehaviour.Properties.of()
-            .mapColor(MapColor.WOOD)
+            .mapColor(mapColor)
             .instrument(NoteBlockInstrument.BASS)
             .strength(2.5F)
             .sound(SoundType.WOOD);
@@ -98,9 +154,9 @@ public class ModBlocks {
         );
     }
 
-    private static Block barrelRegister(String name, Boolean burns) {
+    private static Block barrelRegister(String name, MapColor mapColor, Boolean burns) {
         BlockBehaviour.Properties properties = BlockBehaviour.Properties.of()
-            .mapColor(MapColor.WOOD)
+            .mapColor(mapColor)
             .instrument(NoteBlockInstrument.BASS)
             .strength(2.5F)
             .sound(SoundType.WOOD);
@@ -115,12 +171,12 @@ public class ModBlocks {
         );
     }
 
-    private static Block furnaceRegister(String name) {
+    private static Block furnaceRegister(String name, MapColor mapColor) {
         return register(
             name,
             props -> new ModFurnaceBlock(props, name),
             BlockBehaviour.Properties.of()
-                .mapColor(MapColor.STONE)
+                .mapColor(mapColor)
                 .instrument(NoteBlockInstrument.BASEDRUM)
                 .requiresCorrectToolForDrops()
                 .strength(3.5F)
@@ -129,9 +185,9 @@ public class ModBlocks {
         );
     }
 
-    private static Block craftingTableRegister(String name, Boolean burns) {
+    private static Block craftingTableRegister(String name, MapColor mapColor, Boolean burns) {
         BlockBehaviour.Properties properties = BlockBehaviour.Properties.of()
-            .mapColor(MapColor.WOOD)
+            .mapColor(mapColor)
             .instrument(NoteBlockInstrument.BASS)
             .strength(2.5F)
             .sound(SoundType.WOOD);
