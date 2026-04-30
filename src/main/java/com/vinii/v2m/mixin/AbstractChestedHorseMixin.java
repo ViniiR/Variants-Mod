4116b11c3bb173fc5013be4a8218baff5f55c642
ViniiR <cvrm2004@gmail.com>
2026-04-30 11:21:35 -0300

    feat: mod chests work on donkeys

---
+++ src/main/java/com/vinii/v2m/mixin/AbstractChestedHorseMixin.java
@@ -0,0 +1,24 @@
+package com.vinii.v2m.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.vinii.v2m.datagen.tag.ModItemTagProvider;
+import net.minecraft.world.entity.animal.equine.AbstractChestedHorse;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(AbstractChestedHorse.class)
+public abstract class AbstractChestedHorseMixin {
+    @WrapOperation(
+        method = "mobInteract",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"
+        )
+    )
+    boolean useModChestsOnDonkey(ItemStack instance, Item item, Operation<Boolean> original) {
+        return original.call(instance, item) || instance.is(ModItemTagProvider.MOD_CHESTS);
+    }
+}
