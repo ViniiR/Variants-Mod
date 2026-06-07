78688ee8b297ba36767f9314d93295ad5328ff5d
ViniiR <cvrm2004@gmail.com>
2026-06-06 23:59:33 -0300

    fix: lower icid spawn

--- src/main/java/com/vinii/v2m/world/ModBiomeModifications.java
+++ src/main/java/com/vinii/v2m/world/ModBiomeModifications.java
@@ -1,20 +1,37 @@
 package com.vinii.v2m.world;
 
+import com.vinii.v2m.ViniisVariantsMod;
+import com.vinii.v2m.datagen.tag.ModBiomeTagProvider;
+import com.vinii.v2m.entity.ModEntities;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class ModBiomeModifications {
     public static void initialize() {
-        // TODO: replace zombie spawn on snow biomes
-//        BiomeModifications.addSpawn(
-//            BiomeSelectors.tag(ModBiomeTagProvider.SNOWY_BIOMES),
-//            MobCategory.MONSTER,
-//            ModEntities.ICID,
-//            95,
-//            4,
-//            4
-//        );
+        BiomeModifications.create(Identifier.fromNamespaceAndPath(ViniisVariantsMod.MOD_ID, "icid_spawn"))
+            .add(ModificationPhase.REPLACEMENTS,
+                BiomeSelectors.tag(ModBiomeTagProvider.SNOWY_BIOMES),
+                ctx -> {
+                    ctx.getMobSpawnSettings()
+                        .removeSpawnsOfEntityType(EntityType.ZOMBIE);
+                    ctx.getMobSpawnSettings()
+                        .addSpawn(MobCategory.MONSTER,
+                            new MobSpawnSettings.SpawnerData(
+                                ModEntities.ICID,
+                                1, 4
+                            ),
+                            95
+                        );
+                }
+            );
+
         BiomeModifications.addFeature(
             (biomeSelectionContext -> biomeSelectionContext.getBiomeKey() == Biomes.PALE_GARDEN),
             GenerationStep.Decoration.VEGETAL_DECORATION,
