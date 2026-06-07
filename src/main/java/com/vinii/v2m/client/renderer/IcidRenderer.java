e42a713bf7bb486123865c3c3dca0d4f6fd8776e
ViniiR <cvrm2004@gmail.com>
2026-06-07 13:44:10 -0300

    fix: update baby icid

--- src/main/java/com/vinii/v2m/client/renderer/IcidRenderer.java
+++ src/main/java/com/vinii/v2m/client/renderer/IcidRenderer.java
@@ -6,6 +6,7 @@ import com.vinii.v2m.entity.entities.Icid;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.monster.zombie.BabyDrownedModel;
 import net.minecraft.client.model.monster.zombie.DrownedModel;
 import net.minecraft.client.renderer.entity.AbstractZombieRenderer;
 import net.minecraft.client.renderer.entity.ArmorModelSet;
@@ -17,24 +18,24 @@ import org.jspecify.annotations.NonNull;
 @Environment(EnvType.CLIENT)
 public class IcidRenderer extends AbstractZombieRenderer<Icid, ZombieRenderState, DrownedModel> {
     private static final Identifier ICID_LOCATION = Identifier.fromNamespaceAndPath(ViniisVariantsMod.MOD_ID, "textures/entity/zombie/icid.png");
+    private static final Identifier BABY_ICID_LOCATION = Identifier.fromNamespaceAndPath(ViniisVariantsMod.MOD_ID, "textures/entity/zombie/icid_baby.png");
 
     public IcidRenderer(EntityRendererProvider.Context context) {
         super(
             context,
             new DrownedModel(context.bakeLayer(ModelLayers.DROWNED)),
-            new DrownedModel(context.bakeLayer(ModelLayers.DROWNED_BABY)),
+            new BabyDrownedModel(context.bakeLayer(ModelLayers.DROWNED_BABY)),
             ArmorModelSet.bake(ModelLayers.DROWNED_ARMOR, context.getModelSet(), DrownedModel::new),
-            ArmorModelSet.bake(ModelLayers.DROWNED_BABY_ARMOR, context.getModelSet(), DrownedModel::new)
+            ArmorModelSet.bake(ModelLayers.DROWNED_BABY_ARMOR, context.getModelSet(), BabyDrownedModel::new)
         );
         this.addLayer(new IcidOuterLayer(this, context.getModelSet()));
     }
 
     @Override
     public @NonNull Identifier getTextureLocation(ZombieRenderState zombieRenderState) {
-        return ICID_LOCATION;
+        return zombieRenderState.isBaby ? BABY_ICID_LOCATION : ICID_LOCATION;
     }
 
-    @Override
     public ZombieRenderState createRenderState() {
         return new ZombieRenderState();
     }
