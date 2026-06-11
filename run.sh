3efa963b04e84fd600408ec0882295ec8fb479f1
ViniiR <cvrm2004@gmail.com>
2026-06-10 22:34:14 -0300

    chore: add minecraft version to jar

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-./gradlew build && cp ./build/libs/v2m-0.1.0.jar ~/.local/share/FreesmLauncher/instances/26.1.2/minecraft/mods
+mod_id=$(grep "mod_id" gradle.properties | awk -F "=" '{print $2}')
+mod_version=$(grep "mod_version" gradle.properties | awk -F "=" '{print $2}')
+minecraft_version=$(grep "minecraft_version" gradle.properties | awk -F "=" '{print $2}')
+
+./gradlew build && cp "./build/libs/$mod_id-$mod_version+$minecraft_version.jar" ~/.local/share/FreesmLauncher/instances/26.1.2/minecraft/mods
