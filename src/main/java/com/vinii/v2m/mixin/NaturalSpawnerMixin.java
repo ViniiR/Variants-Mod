66b9f110112cc658fbe181c3a03c1be7694306fc
ViniiR <cvrm2004@gmail.com>
2026-06-20 11:49:41 -0300

    fix: replace mob spawning method

---
+++ src/main/java/com/vinii/v2m/mixin/NaturalSpawnerMixin.java
@@ -0,0 +1,51 @@
+package com.vinii.v2m.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.vinii.v2m.datagen.tag.ModBiomeTagProvider;
+import com.vinii.v2m.entity.ModEntities;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.NaturalSpawner;
+import net.minecraft.world.level.biome.Biomes;
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(NaturalSpawner.class)
+public class NaturalSpawnerMixin {
+    @WrapOperation(
+        method = "spawnCategoryForPosition(Lnet/minecraft/world/entity/MobCategory;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/level/chunk/ChunkAccess;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/NaturalSpawner$SpawnPredicate;Lnet/minecraft/world/level/NaturalSpawner$AfterSpawnCallback;)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/level/NaturalSpawner;getMobForSpawn(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/EntityType;)Lnet/minecraft/world/entity/Mob;"
+        )
+    )
+    private static @Nullable Mob replaceMobWithVariant(
+        ServerLevel level,
+        EntityType<?> type,
+        Operation<Mob> original,
+        @Local(argsOnly = true, name = "start") BlockPos start
+
+    ) {
+        var biome = level.getBiome(start);
+
+        return switch (type) {
+            case EntityType<?> e when e == EntityType.ZOMBIE && biome.is(ModBiomeTagProvider.SNOWY_BIOMES) ->
+                original.call(level, ModEntities.ICID);
+            case EntityType<?> e when e == EntityType.SKELETON && biome.is(Biomes.SOUL_SAND_VALLEY) ->
+                original.call(level, ModEntities.SOUL_SKELETON);
+            case EntityType<?> e when e == EntityType.BLAZE && biome.is(Biomes.SOUL_SAND_VALLEY) ->
+                original.call(level, ModEntities.SOUL_BLAZE);
+            case EntityType<?> e when e == EntityType.ENDERMAN && biome.is(Biomes.WARPED_FOREST) ->
+                original.call(level, ModEntities.WARPED_ENDERMAN);
+            case EntityType<?> e when e == EntityType.GUARDIAN && biome.is(ModBiomeTagProvider.COLD_OCEAN_BIOMES) ->
+                original.call(level, ModEntities.FROZEN_GUARDIAN);
+            default -> original.call(level, type);
+        };
+    }
+}
--- src/main/java/com/vinii/v2m/world/ModBiomeModifications.java
+++ src/main/java/com/vinii/v2m/world/ModBiomeModifications.java
@@ -1,13 +1,7 @@
 package com.vinii.v2m.world;
 
-import com.vinii.v2m.ViniisVariantsMod;
-import com.vinii.v2m.datagen.tag.ModBiomeTagProvider;
-import com.vinii.v2m.entity.ModEntities;
 import net.fabricmc.fabric.api.biome.v1.BiomeModificationContext;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
-import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biomes;
@@ -16,27 +10,6 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class ModBiomeModifications {
     public static void initialize() {
-        BiomeModifications.create(Identifier.fromNamespaceAndPath(ViniisVariantsMod.MOD_ID, "icid_spawn"))
-            .add(ModificationPhase.REPLACEMENTS,
-                BiomeSelectors.tag(ModBiomeTagProvider.SNOWY_BIOMES),
-                ctx ->
-                    replaceSpawn(ctx, EntityType.ZOMBIE, MobCategory.MONSTER, ModEntities.ICID, 1, 4, 95)
-            );
-
-        BiomeModifications.create(Identifier.fromNamespaceAndPath(ViniisVariantsMod.MOD_ID, "soul_skeleton_spawn"))
-            .add(ModificationPhase.REPLACEMENTS,
-                BiomeSelectors.includeByKey(Biomes.SOUL_SAND_VALLEY),
-                ctx ->
-                    replaceSpawn(ctx, EntityType.SKELETON, MobCategory.MONSTER, ModEntities.SOUL_SKELETON, 1, 5, 20)
-            );
-
-        BiomeModifications.create(Identifier.fromNamespaceAndPath(ViniisVariantsMod.MOD_ID, "warped_enderman_spawn"))
-            .add(ModificationPhase.REPLACEMENTS,
-                BiomeSelectors.includeByKey(Biomes.WARPED_FOREST),
-                ctx ->
-                    replaceSpawn(ctx, EntityType.ENDERMAN, MobCategory.MONSTER, ModEntities.WARPED_ENDERMAN, 1, 4, 1)
-            );
-
         BiomeModifications.addFeature(
             (biomeSelectionContext -> biomeSelectionContext.getBiomeKey() == Biomes.PALE_GARDEN),
             GenerationStep.Decoration.VEGETAL_DECORATION,
@@ -44,6 +17,7 @@ public class ModBiomeModifications {
         );
     }
 
+    // TODO: remove
     protected static void replaceSpawn(
         BiomeModificationContext ctx,
         EntityType<?> type,
@@ -51,15 +25,14 @@ public class ModBiomeModifications {
         EntityType<?> newType,
         int min, int max, int weight
     ) {
-        ctx.getMobSpawnSettings()
-            .removeSpawnsOfEntityType(type);
-        ctx.getMobSpawnSettings()
-            .addSpawn(category,
-                new MobSpawnSettings.SpawnerData(
-                    newType,
-                    min, max
-                ),
-                weight
-            );
+        var settings = ctx.getMobSpawnSettings();
+        settings.removeSpawnsOfEntityType(type);
+        settings.addSpawn(category,
+            new MobSpawnSettings.SpawnerData(
+                newType,
+                min, max
+            ),
+            weight
+        );
     }
 }
