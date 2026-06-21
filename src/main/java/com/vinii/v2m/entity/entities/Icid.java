4328a0804d816b1a17df6c2055eb6d07d3846f0b
ViniiR <cvrm2004@gmail.com>
2026-06-21 20:08:42 -0300

    feat: add icid attack effect

--- src/main/java/com/vinii/v2m/entity/entities/Icid.java
+++ src/main/java/com/vinii/v2m/entity/entities/Icid.java
@@ -1,7 +1,11 @@
 package com.vinii.v2m.entity.entities;
 
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.zombie.Zombie;
 import net.minecraft.world.level.Level;
 import org.jspecify.annotations.NonNull;
@@ -14,6 +18,17 @@ public class Icid extends Zombie {
     @Override
     protected boolean isSunSensitive() {
         return false;
+    }
+
+    @Override
+    public boolean doHurtTarget(final @NonNull ServerLevel level, final @NonNull Entity target) {
+        boolean result = super.doHurtTarget(level, target);
+        if (result && this.getMainHandItem().isEmpty() && target instanceof LivingEntity) {
+            float difficulty = level.getCurrentDifficultyAt(this.blockPosition()).getEffectiveDifficulty();
+            ((LivingEntity) target).addEffect(new MobEffectInstance(MobEffects.SLOWNESS, 140 * (int) difficulty), this);
+        }
+
+        return result;
     }
 
     protected void doUnderWaterConversion(@NonNull ServerLevel serverLevel) {
