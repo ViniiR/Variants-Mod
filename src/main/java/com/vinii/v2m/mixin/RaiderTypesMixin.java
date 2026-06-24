4958e6100bb2d59d3721d8660341995c854bceae
ViniiR <cvrm2004@gmail.com>
2026-06-24 14:22:01 -0300

    feat: add natural spawn for raiders

---
+++ src/main/java/com/vinii/v2m/mixin/RaiderTypesMixin.java
@@ -0,0 +1,22 @@
+package com.vinii.v2m.mixin;
+
+import com.vinii.v2m.entity.ModEntities;
+import net.minecraft.world.entity.raid.Raid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net.minecraft.world.entity.raid.Raid$RaiderType")
+public abstract class RaiderTypesMixin {
+    @Inject(
+        method = "<clinit>",
+        at = @At("TAIL")
+    )
+    private static void replaceRaiders(CallbackInfo ci) {
+        // PILLAGER is default
+        Raid.RaiderType.VINDICATOR.entityType = ModEntities.RAID_VINDICATOR;
+        Raid.RaiderType.EVOKER.entityType = ModEntities.RAID_EVOKER;
+        Raid.RaiderType.WITCH.entityType = ModEntities.RAID_WITCH;
+    }
+}
