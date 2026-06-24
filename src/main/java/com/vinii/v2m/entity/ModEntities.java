08f72c038dbe614027cd8bca5ee194a769deb24a
ViniiR <cvrm2004@gmail.com>
2026-06-24 17:58:15 -0300

    style: collapse ModEntities

--- src/main/java/com/vinii/v2m/entity/ModEntities.java
+++ src/main/java/com/vinii/v2m/entity/ModEntities.java
@@ -37,81 +37,23 @@ public class ModEntities {
     }
     // TODO: add spawn eggs
 
-    public static final EntityType<Icid> ICID = register(
-        "icid",
-        EntityType.Builder.of(Icid::new, MobCategory.MONSTER)
-            .sized(0.6F, 1.95F)
-            .eyeHeight(1.74F)
-            .passengerAttachments(2.0125F)
-            .ridingOffset(-0.7F)
-            .clientTrackingRange(8)
-            .notInPeaceful()
-    );
+    public static final EntityType<Icid> ICID = register("icid", EntityType.Builder.of(Icid::new, MobCategory.MONSTER).sized(0.6F, 1.95F).eyeHeight(1.74F).passengerAttachments(2.0125F).ridingOffset(-0.7F).clientTrackingRange(8).notInPeaceful());
+    public static final EntityType<MuddyZombie> MUDDY_ZOMBIE = register("muddy_zombie", EntityType.Builder.of(MuddyZombie::new, MobCategory.MONSTER).sized(0.6F, 1.95F).eyeHeight(1.74F).passengerAttachments(2.0125F).ridingOffset(-0.7F).clientTrackingRange(8).notInPeaceful());
 
-    public static final EntityType<MuddyZombie> MUDDY_ZOMBIE = register(
-        "muddy_zombie",
-        EntityType.Builder.of(MuddyZombie::new, MobCategory.MONSTER)
-            .sized(0.6F, 1.95F)
-            .eyeHeight(1.74F)
-            .passengerAttachments(2.0125F)
-            .ridingOffset(-0.7F)
-            .clientTrackingRange(8)
-            .notInPeaceful()
-    );
+    public static final EntityType<SoulSkeleton> SOUL_SKELETON = register("soul_skeleton", EntityType.Builder.of(SoulSkeleton::new, MobCategory.MONSTER).sized(0.6F, 1.99F).eyeHeight(1.74F).ridingOffset(-0.7F).clientTrackingRange(8).notInPeaceful());
 
-    public static final EntityType<SoulSkeleton> SOUL_SKELETON = register(
-        "soul_skeleton",
-        EntityType.Builder.of(SoulSkeleton::new, MobCategory.MONSTER)
-            .sized(0.6F, 1.99F)
-            .eyeHeight(1.74F)
-            .ridingOffset(-0.7F)
-            .clientTrackingRange(8)
-            .notInPeaceful()
-    );
+    public static final EntityType<WarpedEnderman> WARPED_ENDERMAN = register("warped_enderman", EntityType.Builder.of(WarpedEnderman::new, MobCategory.MONSTER).sized(0.6F, 2.9F).eyeHeight(2.55F).passengerAttachments(2.80625F).clientTrackingRange(8).notInPeaceful());
 
-    public static final EntityType<PaleSnowGolem> PALE_SNOW_GOLEM = register(
-        "pale_snow_golem",
-        EntityType.Builder.of(PaleSnowGolem::new, MobCategory.MISC)
-            .immuneTo(Blocks.POWDER_SNOW)
-            .sized(0.7F, 1.9F)
-            .eyeHeight(1.7F)
-            .clientTrackingRange(8)
-    );
+    public static final EntityType<PaleSnowGolem> PALE_SNOW_GOLEM = register("pale_snow_golem", EntityType.Builder.of(PaleSnowGolem::new, MobCategory.MISC).immuneTo(Blocks.POWDER_SNOW).sized(0.7F, 1.9F).eyeHeight(1.7F).clientTrackingRange(8));
 
-    public static final EntityType<WarpedEnderman> WARPED_ENDERMAN = register(
-        "warped_enderman",
-        EntityType.Builder.of(WarpedEnderman::new, MobCategory.MONSTER)
-            .sized(0.6F, 2.9F)
-            .eyeHeight(2.55F)
-            .passengerAttachments(2.80625F)
-            .clientTrackingRange(8)
-            .notInPeaceful()
-    );
+    public static final EntityType<SoulBlaze> SOUL_BLAZE = register("soul_blaze", EntityType.Builder.of(SoulBlaze::new, MobCategory.MONSTER).fireImmune().sized(0.6F, 1.8F).clientTrackingRange(8).notInPeaceful());
 
-    public static final EntityType<SoulBlaze> SOUL_BLAZE = register(
-        "soul_blaze",
-        EntityType.Builder.of(SoulBlaze::new, MobCategory.MONSTER)
-            .fireImmune()
-            .sized(0.6F, 1.8F)
-            .clientTrackingRange(8)
-            .notInPeaceful()
-    );
+    public static final EntityType<FrozenGuardian> FROZEN_GUARDIAN = register("frozen_guardian", EntityType.Builder.of(FrozenGuardian::new, MobCategory.MONSTER).sized(0.85F, 0.85F).eyeHeight(0.425F).passengerAttachments(0.975F).clientTrackingRange(8).notInPeaceful());
 
-    public static final EntityType<FrozenGuardian> FROZEN_GUARDIAN = register(
-        "frozen_guardian",
-        EntityType.Builder.of(FrozenGuardian::new, MobCategory.MONSTER)
-            .sized(0.85F, 0.85F)
-            .eyeHeight(0.425F)
-            .passengerAttachments(0.975F)
-            .clientTrackingRange(8)
-            .notInPeaceful()
-    );
-
+    public static final EntityType<MountaineerPillager> MOUNTAINEER_PILLAGER = register("mountaineer_pillager", EntityType.Builder.of(MountaineerPillager::new, MobCategory.MONSTER).canSpawnFarFromPlayer().sized(0.6F, 1.95F).passengerAttachments(2.0F).ridingOffset(-0.6F).clientTrackingRange(8).notInPeaceful());
     public static final EntityType<RaidVindicator> RAID_VINDICATOR = register("raid_vindicator", EntityType.Builder.of(RaidVindicator::new, MobCategory.MONSTER).sized(0.6F, 1.95F).passengerAttachments(2.0F).ridingOffset(-0.6F).clientTrackingRange(8).notInPeaceful());
     public static final EntityType<RaidWitch> RAID_WITCH = register("raid_witch", EntityType.Builder.of(RaidWitch::new, MobCategory.MONSTER).sized(0.6F, 1.95F).eyeHeight(1.62F).passengerAttachments(2.2625F).clientTrackingRange(8).notInPeaceful());
     public static final EntityType<RaidEvoker> RAID_EVOKER = register("raid_evoker", EntityType.Builder.of(RaidEvoker::new, MobCategory.MONSTER).sized(0.6F, 1.95F).passengerAttachments(2.0F).ridingOffset(-0.6F).clientTrackingRange(8).notInPeaceful());
-
-    public static final EntityType<MountaineerPillager> MOUNTAINEER_PILLAGER = register("mountaineer_pillager", EntityType.Builder.of(MountaineerPillager::new, MobCategory.MONSTER).canSpawnFarFromPlayer().sized(0.6F, 1.95F).passengerAttachments(2.0F).ridingOffset(-0.6F).clientTrackingRange(8).notInPeaceful());
 
     public static Optional<ResourceKey<LootTable>> copyLootTableFrom(final String entityName) {
         return Optional.of(ResourceKey.create(Registries.LOOT_TABLE, Identifier.withDefaultNamespace(String.format("entities/%s", entityName))));
