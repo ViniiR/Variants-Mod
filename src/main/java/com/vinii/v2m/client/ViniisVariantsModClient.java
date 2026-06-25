776675072c47043bcbcecaa82fe72d93a7eaa491
ViniiR <cvrm2004@gmail.com>
2026-06-24 23:44:49 -0300

    style: collapse renderers

--- src/main/java/com/vinii/v2m/client/ViniisVariantsModClient.java
+++ src/main/java/com/vinii/v2m/client/ViniisVariantsModClient.java
@@ -12,47 +12,20 @@ public class ViniisVariantsModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // Entities
-        EntityRenderers.register(
-            ModEntities.ICID,
-            IcidRenderer::new
-        );
-        EntityRenderers.register(
-            ModEntities.MUDDY_ZOMBIE,
-            MuddyZombieRenderer::new
-        );
-        EntityRenderers.register(
-            ModEntities.SOUL_SKELETON,
-            SoulSkeletonRenderer::new
-        );
-        EntityRenderers.register(
-            ModEntities.PALE_SNOW_GOLEM,
-            PaleSnowGolemRenderer::new
-        );
-        EntityRenderers.register(
-            ModEntities.WARPED_ENDERMAN,
-            WarpedEndermanRenderer::new
-        );
-        EntityRenderers.register(
-            ModEntities.FROZEN_GUARDIAN,
-            FrozenGuardianRenderer::new
-        );
-        EntityRenderers.register(
-            ModEntities.SOUL_BLAZE,
-            SoulBlazeRenderer::new
-        );
+        EntityRenderers.register(ModEntities.ICID, IcidRenderer::new);
+        EntityRenderers.register(ModEntities.MUDDY_ZOMBIE, MuddyZombieRenderer::new);
+        EntityRenderers.register(ModEntities.SOUL_SKELETON, SoulSkeletonRenderer::new);
+        EntityRenderers.register(ModEntities.PALE_SNOW_GOLEM, PaleSnowGolemRenderer::new);
+        EntityRenderers.register(ModEntities.WARPED_ENDERMAN, WarpedEndermanRenderer::new);
+        EntityRenderers.register(ModEntities.FROZEN_GUARDIAN, FrozenGuardianRenderer::new);
+        EntityRenderers.register(ModEntities.SOUL_BLAZE, SoulBlazeRenderer::new);
         EntityRenderers.register(ModEntities.RAID_VINDICATOR, RaidVindicatorRenderer::new);
         EntityRenderers.register(ModEntities.RAID_WITCH, RaidWitchRenderer::new);
         EntityRenderers.register(ModEntities.RAID_EVOKER, RaidEvokerRenderer::new);
         EntityRenderers.register(ModEntities.MOUNTAINEER_PILLAGER, MountaineerPillagerRenderer::new);
 
         // Block entities
-        BlockEntityRenderers.register(
-            ModBlockEntities.MOD_CHEST_BLOCK_ENTITY,
-            ChestRenderer::new
-        );
-        BlockEntityRenderers.register(
-            ModBlockEntities.MOD_TRAPPED_CHEST_BLOCK_ENTITY,
-            ChestRenderer::new
-        );
+        BlockEntityRenderers.register(ModBlockEntities.MOD_CHEST_BLOCK_ENTITY, ChestRenderer::new);
+        BlockEntityRenderers.register(ModBlockEntities.MOD_TRAPPED_CHEST_BLOCK_ENTITY, ChestRenderer::new);
     }
 }
