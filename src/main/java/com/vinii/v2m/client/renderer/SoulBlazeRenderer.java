b7b7468fdaab90dcb809d4b61f363d8a7335e327
ViniiR <cvrm2004@gmail.com>
2026-06-25 11:56:38 -0300

    chore: add annotation to all client classes

--- src/main/java/com/vinii/v2m/client/renderer/SoulBlazeRenderer.java
+++ src/main/java/com/vinii/v2m/client/renderer/SoulBlazeRenderer.java
@@ -1,12 +1,15 @@
 package com.vinii.v2m.client.renderer;
 
 import com.vinii.v2m.ViniisVariantsMod;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.BlazeRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.resources.Identifier;
 import org.jspecify.annotations.NonNull;
 
+@Environment(EnvType.CLIENT)
 public class SoulBlazeRenderer extends BlazeRenderer {
     public SoulBlazeRenderer(EntityRendererProvider.Context context) {
         super(context);
